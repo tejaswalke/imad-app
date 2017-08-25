106d79e9035d6284151083b4b81388e9d24ea4a7
Tejas Walke <tejaswalke927@gmail.com>
2017-08-25 13:56:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,8 +20,6 @@ app.get('/article-two', function(req, res) {
 app.get('/article-three', function(req, res) {
     res.send('Article three will be requested and served here');
 });
-
-}
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
