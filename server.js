450d44664e3c52e8bc9b54b897c5964ad31c55b3
Tejas Walke <tejaswalke927@gmail.com>
2017-08-26 13:00:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,53 +1,127 @@
-<html>
-    <head>
-        <title>
-            Article-One  | Tejas Walke
-        </title>
-        <meta name="viewport" content="width=device-Width, initial-scale=1" />
- 
-        <link href="/ui/style.css" rel="stylesheet" />
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
-    </head>
+var app = express();
+app.use(morgan('combined'));
 
-    <body>
-        <div class='container'>
-            <div>
-                <a href="/">Home</a>
-            </div>
-            <hr/>
-            <h3>
-                Article Three
-            </h3>
-            <div>
-                 Dec 17
-            </div>
-            <div>
-                    <p>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
-                        
-                    </p>
+var articleOne={
+    title:' Article-One  | Tejas Walke',
+    heading:'Article One',
+    date:'Dec 12, 2017',
+    content: `
+                <p>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
                     
-                    <p>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
+                </p>
+                
+                <p>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    
+                </p>
+                
+                <p>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                    
+                </p>
+    `
+    
+};
+
+function createTemplate (data) {
+    var title= data.title;
+    var date= data.date;
+    var heading=data.heading;
+    var content=data.content;
+    var htmlTemplate=   `
+                        <html>
+                        <head>
+                            <title>
+                                ${title}
+                            </title>
+                            
+                            <meta name="viewport" content="width=device-Width, initial-scale=1" />
+                            
+                            <link href="/ui/style.css" rel="stylesheet" />
+                        </head>
                         
-                    </p>                
-                    <p>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
-                        
-                    </p>
-              </div>
-         </div>
-    </body>
-</html>
+                        <body>
+                            <div class='container'>
+                                    
+                                    <div>
+                                        <a href="/">Home</a>
+                                    </div>
+                                    <hr/>
+                                    <h3>
+                                        ${heading}
+                                    </h3>
+                                    <div>
+                                         ${date}
+                                    </div>
+                                    <div>
+                                        ${content}
+                                     </div>
+                           
+                                 </div>  
+                                 
+                            </body>
+                            
+                        </html>
+                    
+
+                    `;
+                    return htmlTemplate;
+}
+
+app.get('/', function (req, res) {
+  res.send(createTemplate(articleOne));
+});
+
+app.get('/article-one', function(req,res) {
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
+app.get('/article-two', function(req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+
+app.get('/article-three', function(req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
+
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
