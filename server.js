b48778cca9f877dbbe47ef71ae5029dd1163c01c
Tejas Walke <tejaswalke927@gmail.com>
2017-08-26 12:26:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,9 +93,9 @@ app.get('/',function(req,res){
 });
 
 app.get('/:articleName', function (req, res) {
-    //articleName==article-one
-    //articles[articleName]=={} content object for article one
-    var articleName=req.params.articleName;
+    // articleName == article-one
+    // articles[articleName] == {} content object for article one
+    var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
  
 });
