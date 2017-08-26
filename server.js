13cb0958024d1d3c5d84030e8b95f632777b3c67
Tejas Walke <tejaswalke927@gmail.com>
2017-08-26 12:59:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,11 +3,12 @@
         <title>
             Article-One  | Tejas Walke
         </title>
-        
         <meta name="viewport" content="width=device-Width, initial-scale=1" />
-        
+ 
         <link href="/ui/style.css" rel="stylesheet" />
+
     </head>
+
     <body>
         <div class='container'>
             <div>
@@ -15,45 +16,38 @@
             </div>
             <hr/>
             <h3>
-                Article One
+                Article Three
             </h3>
             <div>
-                 Dec 12
+                 Dec 17
             </div>
             <div>
                     <p>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
                         
                     </p>
                     
                     <p>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
                         
-                    </p>
-                    
+                    </p>                
                     <p>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
-                        This is the content of my First Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
+                        This is the content of my Third Article :-) This content is edited by Tejas Walke<br>
                         
                     </p>
-             </div>
-        </div>         
+              </div>
+         </div>
     </body>
 </html>
