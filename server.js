56ed32d9efe5ad5389fa7d070269182a1df5a368
Tejas Walke <tejaswalke927@gmail.com>
2017-08-27 12:44:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles={
-            'article-One':{
+            'articleOne':{
             title: 'Article One | Tejas Walke',
             heading:'Article One',
             date:'Dec 12',
